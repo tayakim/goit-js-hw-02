c98e003d0f5b816d2f55dde678232a3b06548b0a
Tatiana <taulru@yandex.com>
2020-11-12 00:46:52 +0100

    added new tasks

--- Module2.js
+++ Module2.js
@@ -31,13 +31,10 @@
 
 // Task #2
 
-const calculateEngravingPrice = (message = "", pricePerWord = 0) => pricePerWord * message.split(' ').length;
+// const calculateEngravingPrice = (message = "", pricePerWord = 0) => pricePerWord * message.split(' ').length;
 
 
-
-
-
-console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
 
 //console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
 
@@ -46,3 +43,71 @@ console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient m
 //console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
 
 //console.log(calculateEngravingPrice('Uno', 100)); // 100
+
+// Task #3
+// Ищем самое длинное слово в array
+// function findLongestWord(string = "") {
+//     let stringArray = string.split(' ');
+//     // console.log(stringArray.length);
+//     // console.log(stringArray);
+//     let longestWord = stringArray[0];
+//     // console.log(longestWord);
+//     for (let i = 0; i < stringArray.length - 1; i += 1) {
+//         if (longestWord.length < stringArray[i + 1].length) {
+//             longestWord = stringArray[i + 1];
+//         }
+//     }
+//     return longestWord;
+// }
+
+// console.log(findLongestWord('May the force be with you'));
+// // 'force'
+
+// Task #4
+
+// function formatString(string, maxLength = 40) {
+//     if (string.length <= maxLength) {
+//         return string;
+
+//     }
+//     if (string.length > maxLength) {
+
+
+//         let cutString = string.split('', maxLength);
+//         let elem = "...";
+//         let newArray = cutString.splice(maxLength + 1, 0, elem);
+//         const newSrting = cutString.concat(newArray);
+//         let finalString = newSrting.join('');
+//         return finalString;
+//     }
+// }
+
+
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+// 'Vestibulum facilisis, purus ne...'
+
+// Task #5
+
+function checkForSpam(str) {
+    'use strict';
+    if (str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
