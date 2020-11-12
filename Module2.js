f621abed4b3c88726f3afd499d9699de05a74280
Tatiana <taulru@yandex.com>
2020-11-12 21:05:42 +0100

    added HW2

--- Module2.js
+++ Module2.js
@@ -94,20 +94,147 @@
 
 // Task #5
 
-function checkForSpam(str) {
-    'use strict';
-    if (str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale')) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// function checkForSpam(str) {
+//     'use strict';
+//     if (str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale')) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
 
-console.log(checkForSpam('Latest technology news')); // false
+// console.log(checkForSpam('Latest technology news')); // false
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+// console.log(checkForSpam('Get best sale offers now!')); // true
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
+// Task #6
+// function mapArray(array) {
+//     'use strict';
+//     const numbers = new Array(array.length);
+
+//     for (let i = 0; i < array.length; i += 1) {
+//         // let mul = array[i] * 10;
+//         // console.log(mul);
+//         numbers[i] = array[i] * 10;
+//     }
+//     return numbers;
+// }
+
+// console.log(mapArray([-2, 0, 2]));
+// [-20, 0, 20]
+
+//console.log(mapArray([-2.5, 0, 2.5]));
+// [-25, 0, 25]
+
+
+// Task #7
+
+// function filterArray(array) {
+//     'use strict';
+//     const numbers = [];
+//     for (let i = 0; i < array.length; i += 1) {
+//         if (Number.isFinite(array[i]) === true) {
+//             numbers.push(array[i])
+//         }
+
+//     }
+//     return numbers;
+// }
+
+// console.log(filterArray([-2, 0, 2]));
+// // [-2, 0, 2]
+
+// console.log(filterArray([1, NaN, Infinity]));
+// // [1]
+
+// console.log(filterArray([0, -0, 100, '100']));
+// // [0, 0, 100]
+
+// console.log(filterArray([undefined, false, null, [], 1]));
+// // [1]
+
+// console.log(filterArray([{}, () => {}, 2]));
+// // [2]
+
+// Task #8
+// function reduceArray(array) {
+//     'use strict';
+//     let total = 0;
+//     if (array === 0) {
+//         console.log("0");
+//     } else {
+//         for (let i = 0; i < array.length; i += 1) {
+//             total += array[i];
+//         }
+//     }
+
+//     return total;
+// };
+
+
+// console.log(reduceArray([1, 2, 3]));
+// 6
+
+// console.log(reduceArray([-2, 0, 2]));
+// // 0
+
+// console.log(reduceArray([1, 2, 2.5]));
+// // 5.5
+
+// Task #9
+
+// function isLoginValid(login, min = 4, max = 16) {
+//     if ((login.length >= min) && (login.length <= max)) {
+//         return true;
+
+//     } else {
+//         return false;
+//     }
+// }
+
+// function isLoginUnique(allLogins, login) {
+//     'use strict';
+//     for (let i = 0; i < allLogins.length; i += 1) {
+//         if (allLogins[i] === login) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function addLogin(allLogins, login) {
+//     'use strict';
+//     const SUCCESS = 'Логин успешно добавлен!';
+//     const REFUSAL = 'Такой логин уже используется!';
+//     const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
+//     let message;
+//     if (isLoginValid(login) && isLoginUnique(allLogins, login)) {
+//         allLogins.push(login);
+//         message = SUCCESS;
+//     } else if (isLoginUnique(allLogins, login) === false) {
+//         message = REFUSAL;
+//     } else {
+//         message = ERROR;
+//     }
+//     return message;
+// }
+
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+// console.log(addLogin(logins, 'Ajax'));
+// // 'Логин успешно добавлен!'
+// console.log(logins);
+// console.log(addLogin(logins, 'robotGoogles'));
+// // 'Такой логин уже используется!'
+// console.log(logins);
+// console.log(addLogin(logins, 'Zod'));
+// // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(addLogin(logins, 'jqueryisextremelyfast'));
+// // 'Ошибка! Логин должен быть от 4 до 16 символов'
